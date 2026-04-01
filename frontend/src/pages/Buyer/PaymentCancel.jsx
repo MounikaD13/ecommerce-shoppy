9189d0dfe3209b7bea5256d9153a46101d6193db
MounikaD13 <mounika.d3945@gmail.com>
2026-04-01 21:17:50 +0530

    first commit

---
+++ frontend/src/pages/Buyer/PaymentCancel.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function PaymentCancel() {
+  return (
+    <div>
+      <h2>Payment cancelled</h2>
+      <p>Your payment was cancelled</p>
+    </div>
+  )
+}
