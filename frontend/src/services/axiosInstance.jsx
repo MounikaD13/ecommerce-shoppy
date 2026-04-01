08e6128a6f61b91900842f93c0b21de63a89cec9
MounikaD13 <mounika.d3945@gmail.com>
2026-04-01 21:54:44 +0530

    1

--- frontend/src/services/axiosInstance.jsx
+++ frontend/src/services/axiosInstance.jsx
@@ -1,36 +1,36 @@
 import axios from 'axios'
 const instance = axios.create({
-    baseURL:"http://localhost:2000/api",
-    withCredentials:true
+    baseURL: `${import.meta.env.VITE_API_URL}`, 
+    withCredentials: true
 })
-instance.interceptors.request.use((config)=>{
-    const token= localStorage.getItem("token")
+instance.interceptors.request.use((config) => {
+    const token = localStorage.getItem("token")
     console.log(token)
-    if(token){
-        config.headers.Authorization=`Bearer ${token}`
+    if (token) {
+        config.headers.Authorization = `Bearer ${token}`
     }
     return config
 })
 
 instance.interceptors.response.use(
-    res=>res,
-    async error=>{
-        const originalRequest= error.config 
-        if(error.response.status==400 && !originalRequest._retry){
-            originalRequest._retry=true 
-            try{
-                await axios.post("http://localhost:2000/api/refresh-token",{},{
-                    withCredentials:true
+    res => res,
+    async error => {
+        const originalRequest = error.config
+        if (error.response.status == 400 && !originalRequest._retry) {
+            originalRequest._retry = true
+            try {
+                await axios.post("http://localhost:2000/api/refresh-token", {}, {
+                    withCredentials: true
                 })
-                .then((res)=>{
-                    localStorage.setItem("token",res.data.accessToken)
-                    originalRequest.headers.Authorization=`Bearer ${res.data.accessToken}`
-                    return instance(originalRequest)
-                })
+                    .then((res) => {
+                        localStorage.setItem("token", res.data.accessToken)
+                        originalRequest.headers.Authorization = `Bearer ${res.data.accessToken}`
+                        return instance(originalRequest)
+                    })
             }
-            catch(err){
+            catch (err) {
                 localStorage.removeItem("token")
-                window.location.href="/login"
+                window.location.href = "/login"
             }
         }
         return Promise(error)
