fd40102a8af977de6bc1925f2485a0d380d5c5a6
MounikaD13 <mounika.d3945@gmail.com>
2026-04-02 12:29:09 +0530

    2

--- frontend/src/services/axiosInstance.jsx
+++ frontend/src/services/axiosInstance.jsx
@@ -1,6 +1,6 @@
 import axios from 'axios'
 const instance = axios.create({
-    baseURL: `${import.meta.env.VITE_API_URL}`, 
+    baseURL: `${import.meta.env.VITE_API_URL}`,
     withCredentials: true
 })
 instance.interceptors.request.use((config) => {
