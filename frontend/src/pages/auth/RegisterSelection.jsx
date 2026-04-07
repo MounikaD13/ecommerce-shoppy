927d1fc681197a301c0a3105b9627d06744354d5
MounikaD13 <mounika.d3945@gmail.com>
2026-04-07 18:16:01 +0530

    2

--- frontend/src/pages/auth/RegisterSelection.jsx
+++ frontend/src/pages/auth/RegisterSelection.jsx
@@ -240,10 +240,10 @@ const RegisterSelection = () => {
                     {/* Brand */}
                     <div className="rs-brand">
                         <div className="rs-brand-icon">
-                            <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                            <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" fill="#f7f5f2">
                                 <path d="M6 2 3 6v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2V6l-3-4z"/>
-                                <line x1="3" y1="6" x2="21" y2="6" stroke="#f7f5f2" strokeWidth="1.5" fill="none"/>
-                                <path d="M16 10a4 4 0 0 1-8 0" stroke="#f7f5f2" strokeWidth="1.5" fill="none"/>
+                                <line x1="3" y1="6" x2="21" y2="6" stroke="#1a1a1a" strokeWidth="1.5" fill="none"/>
+                                <path d="M16 10a4 4 0 0 1-8 0" stroke="#1a1a1a" strokeWidth="1.5" fill="none"/>
                             </svg>
                         </div>
                         <span className="rs-brand-name">Shopique</span>
